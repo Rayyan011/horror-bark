463f54458d319507c41b9e0c5473a1f3923490c0
Aidan Afeef <aidanafeef22@gmail.com>
2025-03-18 20:11:01 +0500

    Updated home page and routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
-<<<<<<< HEAD
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ThemeParkController;
@@ -12,15 +11,7 @@ Route::redirect('/', '/home')->name('home');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 Route::get('/theme-park', [ThemeParkController::class, 'index'])->name('theme-park');
-=======
-
-Route::redirect('/', '/home')->name('home');
->>>>>>> 132dbe4e7633d85d880c9b2366a2ee5414c1e904
 Route::get('/{page_name}', [PagesController::class, 'show']);
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-<<<<<<< HEAD
-
-=======
->>>>>>> 132dbe4e7633d85d880c9b2366a2ee5414c1e904
